ec273901c58350ede455c7eece7896456e0433c9
Ehab Alsharif <ehab@myndlift.com>
2019-06-29 15:24:32 +0300

    added sourcemaps to build configuration

--- build/build.js
+++ build/build.js
@@ -1,17 +1,15 @@
-import banner from './banner'
-import babel from 'rollup-plugin-babel'
+import banner from "./banner";
+import babel from "rollup-plugin-babel";
 
 export default {
   output: {
-    name: 'Glide',
-    banner
+    name: "Glide",
+    banner,
+    sourcemap: "inline"
   },
   plugins: [
     babel({
-      plugins: [
-        'external-helpers',
-        'transform-object-assign'
-      ]
+      plugins: ["external-helpers", "transform-object-assign"]
     })
   ]
-}
+};
